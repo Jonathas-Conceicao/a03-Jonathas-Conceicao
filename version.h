6bc492814d9cb731f59b7f9f9ae808af0aebeaf4
Lucas E. Bretana <lucasbretana@gmail.com>
2017-07-31 17:09:57 -0300

    Better to read

--- version.h
+++ version.h
@@ -31,17 +31,17 @@ typedef int indice_fs_t;
  *  Também deve retornar erro caso o número de blocos não seja o suficiente
  *  para armazenar os metadados pelo menos.
  *
- *  @param arquivo nome do arquivo no sistema de arquivos hospedeiro
- *  @param blocos número de blocos do arquivo
- *  @return SUCCESSO ou FALHA
+ *  @param  arquivo   nome do arquivo no sistema de arquivos hospedeiro
+ *  @param  blocos    número de blocos do arquivo
+ *  @return           SUCCESSO ou FALHA
  */
 int initfs(char * arquivo, int blocos);
 
 
 /** Abre um sistema de arquivos.
  *
- *  @param nome do arquivo no sistema de arquivos hospedeiro
- *  @return file handle do descritor de sistema de arquivos ou FALHA
+ *  @param  arquivo   nome do arquivo no sistema de arquivos hospedeiro
+ *  @return           file handle do descritor de sistema de arquivos ou FALHA
  */
 
 indice_fs_t vopenfs(char * arquivo);
@@ -50,27 +50,27 @@ indice_fs_t vopenfs(char * arquivo);
 /** Abre um arquivo versionado.
  *
  *
- * @param fs o file handle do sistema de arquivos
- * @param nome o arquivo a ser aberto
- * @param acesso LEITURA, ESCRITA ou LEITURAESCRITA
- * @param versao versão a ser aberta
- * @return índice do arquivo aberto, FALHA se não abrir
+ * @param   fs        o file handle do sistema de arquivos
+ * @param   nome      o arquivo a ser aberto
+ * @param   acesso    LEITURA, ESCRITA ou LEITURAESCRITA
+ * @param   versao    versão a ser aberta
+ * @return            índice do arquivo aberto, FALHA se não abrir
  */
 indice_arquivo_t vopen(indice_fs_t fs, char * nome,  int acesso, int version);
 
 /** Fecha um arquivo versionado.
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @return SUCESSO OU FALHA
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @return            SUCESSO OU FALHA
  */
 int vclose(indice_arquivo_t arquivo);
 
 /** Lê bytes de um arquivo versionado aberto.
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @param tamanho número de bytes a serem lidos
- * @param buffer ponteiro para buffer onde serão armazenados os bytes lidos
- * @return número de bytes lidos
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @param   tamanho   número de bytes a serem lidos
+ * @param   buffer    ponteiro para buffer onde serão armazenados os bytes lidos
+ * @return            número de bytes lidos
  */
 uint32_t vread(indice_arquivo_t arquivo, uint32_t tamanho, char *buffer);
 
@@ -78,49 +78,49 @@ uint32_t vread(indice_arquivo_t arquivo, uint32_t tamanho, char *buffer);
  *
  * A escrita é sempre realizada no modo append, ou seja, no fim do arquivo.
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @param tamanho número de bytes a serem escritos
- * @param buffer ponteiro para conteúdo a ser escrito
- * @return SUCESSO ou FALHA
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @param   tamanho   número de bytes a serem escritos
+ * @param   buffer    ponteiro para conteúdo a ser escrito
+ * @return            SUCESSO ou FALHA
  */
 int vwrite(indice_arquivo_t arquivo, uint32_t tamanho, char *buffer);
 
 /** Apaga um arquivo e o fecha.
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @return SUCESSO ou FALHA
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @return            SUCESSO ou FALHA
  */
 
 int vdelete(indice_arquivo_t arquivo);
 
 /** Modifica a posição atual de leitura ou escrita do arquivo
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @param seek deslocamento em relação ao início do arquivo
- * @return SUCESSO ou FALHA
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @param   seek      deslocamento em relação ao início do arquivo
+ * @return            SUCESSO ou FALHA
  */
 int vseek(indice_arquivo_t arquivo, uint32_t seek);
 
 /** Retorna o tempo em que uma versão do arquivo foi criado
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @param versao versão a ser retornada
- * @return tempo
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @param   versao    versão a ser retornada
+ * @return            tempo
  */
 time_t vcreation(indice_arquivo_t arquivo, int versao);
 
 /** Retorna o tempo em que a versão do arquivo foi acessado
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @return tempo
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @return            tempo
  */
 time_t vaccessed(indice_arquivo_t arquivo, int versao);
 
 /** Retorna o tempo em que o arquivo foi modificado
  *
- * @param arquivo índice para a tabela de arquivos abertos
- * @param versao versão a ser retornada
- * @return tempo
+ * @param   arquivo   índice para a tabela de arquivos abertos
+ * @param   versao    versão a ser retornada
+ * @return            tempo
  */
 time_t vlast_modified(indice_arquivo_t arquivo, int versao);
 
